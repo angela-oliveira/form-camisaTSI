c7e852670ef2f9d636a28a5aebb09a8430a40a60
angela-oliveira <angela.oliveira.rcc@gmail.com>
2019-09-10 20:45:20 -0300

    resposive

--- script/main.js
+++ script/main.js
@@ -1,20 +1,41 @@
-const resultado = document.querySelector('#resultado01')
+// const resultado = document.querySelector('#resultado01')
+// const mais = document.querySelector('.mais01')
+// const menos = document.querySelector('.menos01')
+// indn(resultado)
 
-const mais = document.querySelector('.mais01')
-const menos = document.querySelector('.menos01')
+// function indn(result){
+//     mais.onclick = ()=> {
+//         if(result.value >= 0){
+//             result.value ++
+//         }
+//     }
+//     menos.onclick =()=>{
+//         if(result.value > 0){
+//             result.value --
+//         }
+//     }
+// }
 
-indn(resultado)
-
-function indn(result){
-
-    mais.onclick = ()=> {
-        if(result.value >= 0){
-            result.value ++
+let cards = document.querySelectorAll('.card');
+for (card of cards) {
+    let resultado = card.querySelector("input");
+    let mais;
+    let menos;
+    
+    links = card.querySelectorAll("a");
+    for (link of links) {
+        if (link.getAttribute('class').indexOf('menos') >= 0) {
+            menos = link;
+        }else{
+            mais = link;
         }
     }
+    mais.onclick = ()=> {
+        resultado.setAttribute('value', Number(resultado.value) + 1);
+    }
     menos.onclick =()=>{
-        if(result.value > 0){
-            result.value --
+        if(resultado.value > 0){
+            resultado.setAttribute('value', Number(resultado.value) - 1);
         }
     }
 }
